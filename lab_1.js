317525119e26b6a1bcc86fb2934edd9de41ba47d
Valeria-CPaz <21214@student.dorset-college.ie>
2020-12-15 15:45:59 +0000

    commit

---
+++ lab_1.js
@@ -0,0 +1,26 @@
+// declaring variables
+var firstName = "Valeria";
+var favouriteColour = "yellow";
+var favouriteFood = "sushi";
+var city = "Sao Paulo";
+
+// printing on console my name
+console.log("My name is " + firstName);
+// printing my favourite colour
+console.log("My favourite colour is " + favouriteColour);
+// printing my favourite food and my city
+console.log("I love " + favouriteFood + " and I'm from " + city);
+
+var phrase = "A rainbow is formed by sunlight. Shinning through rain drops so small. The rain bends the light from the sinshine, and colours the sky for us all.";
+var endingPhrase = "is what we see. Rainbow colours, are made with light bending you see!";
+var one = "red";
+var two = "orange";
+var three = "yellow";
+var four = "green";
+var five = "blue";
+var six = "indigo";
+var seven = "violet";
+
+console.log(phrase + ", " + one + ", " + two + ", " + three + ", " + four + ", " + five + ", " + six + ", " +
+seven + " " + endingPhrase)
+
